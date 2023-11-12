cb1e6e153e87963605e68739ef97adfc4c18be7f
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-11-12 09:55:15 +0530

    added encryption to user password

--- EXPENSE-TRACKER/controllers/user.js
+++ EXPENSE-TRACKER/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt')
 
 const isStringValid = (string) => {
     if(!string || string.length ===0){
@@ -13,15 +14,14 @@ exports.postAddUser = async (req,res) => {
        if(isStringValid(name) || isStringValid(email) || isStringValid(password)){
             return res.status(400).json({err:"Bad parameter. something missing"})
        }
-
-        let newUser = await User.create({
-            name,
-            email,
-            password
-        });
-        
-
-        res.status(201).json({message: 'Successfuly create new user'});
+       bcrypt.hash(password , saltrounds , async (err , hash) => {
+            await User.create({
+                name,
+                email,
+                password : hash
+            });
+            res.status(201).json({message: 'Successfuly create new user'});
+       })
     }catch(err) {
         res.status(403).json({err});
     }
@@ -30,13 +30,29 @@ exports.postAddUser = async (req,res) => {
 exports.login = async (req , res) => {
     try{
         const {email, password} = req.body;
-        const user = await User.findOne({where:{email: email}});
-        if(user === null || user.password != password){
-            return res.status(400).json({err:"user not found"});
+
+        if(isStringValid(email) || isStringValid(password)){
+            return res.status(400).json({success:false ,message:'Email id or password is missing'})
         }
-        res.status(201).json({message: 'Successfuly logged in'});
+
+        const user = await User.findAll({where:{email}});
+        if(user.length > 0){
+            bcrypt.compare(password , user[0].password , (err,result) =>{
+                if(err){
+                    throw new Error('Something went wrong');
+                }
+                if(result){
+                    res.status(200).json({success: true , message: "user logged in successfully"})
+                }else{
+                    return res.status(400).json({success:false,message:'Password is incorrect'})
+                }
+            })
+        }else{
+            return res.status(404).json({success:false , message:'User Does not exist'})
+        }
+        
     }
     catch(err){
-        res.status(403).json({err});
+        res.status(500).json({message:err, success:false});
     }
 }
